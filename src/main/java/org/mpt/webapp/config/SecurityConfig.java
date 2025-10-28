180a032544c244425b4d6534de3aa90e66482264
Pablo1qaz <147703596+Pablo1qaz@users.noreply.github.com>
2025-10-28 19:15:28 +0100

    init commit

---
+++ src/main/java/org/mpt/webapp/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package org.mpt.webapp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/home/**").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("/company/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/", true)
+
+                )
+                .logout(config -> config.logoutSuccessUrl("/"))
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
